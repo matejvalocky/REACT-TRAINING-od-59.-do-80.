c7d179c6da9760d28229573c14784d4eaf11b0c4
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2023-03-12 22:25:48 +0100

    React 61

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const App = () => {
   
 
   const [value, setValue] = useState(0)
+  const [test, setTest] = useState(0)
 
   useEffect( () => {
     const button = document.querySelector(".btn")
@@ -15,17 +16,20 @@ const App = () => {
       button.textContent = "Klikni"
     }
    
-    
-  },[]) // ak doplním ,[] medzi zátvorky, tak sa useEffect spustí iba raz
+    console.log("Prvý useEffect = klikni")
+  },[value]) // ak doplním ,[] medzi zátvorky, tak sa useEffect spustí iba raz
 
-  
+  useEffect ( () => {
+    document.title = `Nový titulok ${test}`
+    console.log("Druhý useEffect = titulok")
+  },[test])
 
   return (
     <div> 
       <h1>začíname</h1>
       <p>{value}</p>
-      {console.log("ja som Return")}
       <button className="btn" onClick={ () => setValue(value + 1)}>Klikni</button>
+      <button className="btn-test" onClick={ () => setTest(test + 1) }>Titulok</button>
     </div>
    
   )
