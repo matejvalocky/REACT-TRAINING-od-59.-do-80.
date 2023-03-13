7d7ec0be8ee11dfcc809309976600e577bef2f68
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2023-03-13 22:33:15 +0100

    React 68  useState a useEffect spolu rieši problém

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-// React 67 - Přidáváme useState
+// React 68 - useState a useEffect dohromady řeší náš problém
 
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const App = () => {
   const [quote, setQuote] = useState("východzí text")
   const url = "https://api.kanye.rest/" // URL NA API
-  
+
 
 
   const getQuote = async () => { 
     const response = await fetch(url) 
     const data = await response.json() 
     const finalQuote = data["quote"] // do finalQuote sa nastaví citát
-    // setQuote(finalQuote)
+    setQuote(finalQuote)
     
 
   }
 
-  getQuote() // ZAVOLANIE FUNKCIE
+  useEffect( () => {
+    getQuote() // ZAVOLANIE FUNKCIE
+  },[]) // funkcia, ktorú obaľuje useEffect sa prevedie iba raz lebo sme pridali na konci ,[]
+
+  
 
 
   return ( // tu prebieha renderovanie
