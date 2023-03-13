dd71de6fd1f6924e94ac296fb3c82db1cba75594
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2023-03-13 23:00:51 +0100

    React 70 vypísanie suradn.  ISS  do str. nefunguje

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
-//React 69 - API pre medzinárodnú vesmírnu stanicu
+//React 70 vypísanie zem. šírky a dlžky ISS do stránky nefunguje
 
 const App = () => {
 
   const url = "http://api.open-notify.org/iss-now.json"
+
+  let latitude = ""
+  let longitude = ""
 
   const getCoordinates = async() => {
     const response = await fetch(url)
     const data = await response.json() // než spustíš tento riadok, počkaj kým sa dokončí riadok nad ním
     console.log(data["iss_position"]["latitude"]) // zemepisná šírka
     console.log(data["iss_position"]["longitude"]) // zemepisná dĺžka
+    latitude = data["iss_position"]["latitude"]
+    longitude = data["iss_position"]["longitude"]
   }
 
   getCoordinates() // zavolanie funkcie
 
   return (
-    <h1>API</h1>
+    <div>
+      <h1>API</h1>
+      <h2>Zemepisná šírka</h2>
+      <p>{latitude}</p>
+      <h2>Zemepisná dĺžka</h2>
+      <p>{longitude}</p>
+    </div>
   )
 }
 
