83598df20c4e8c17ece64a8b1c8719d7a3e34fd0
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2023-03-21 21:37:55 +0100

    React 73 - Multiple Returns

--- src/App.js
+++ src/App.js
@@ -1,50 +1,16 @@
-//React 72 Vylepšujeme stránku o odkaz do máp s aktuálnou pozíciou ISS
+// React 73 - Multiple Returns
+// druhy return nebude nikdy spustený = unreacheable code = nedostiahnuteľný kód
 
-// potrebujeme refreshnut, teda znova vypísať komponentu do stránky = useState
-
-import { useState, useEffect} from "react"
 
 const App = () => {
+  const loading = false
 
-  const url = "http://api.open-notify.org/iss-now.json"
-
-  // 1. nastavenie latitude a longitude na prázdne stringy cez useState
-
-  const [latitude, setLatitude] = useState("") // prázdny string sa nastaví do latitude
-  const [longitude, setLongitude] = useState("") // prázdny string sa nastaví do longitude
-  const [urlMap, setUrlMap] = useState("")
-
-  
-
-  const getCoordinates = async() => { //3. zavolanie funkcie, vďaka nej latitute a logtitude nastav na správne hodnoty a potom to všetko vyrenderuj do stránky
-    const response = await fetch(url)
-    const data = await response.json() // než spustíš tento riadok, počkaj kým sa dokončí riadok nad ním
-
-    setLatitude(data["iss_position"]["latitude"])
-    setLongitude(data["iss_position"]["longitude"])
-    const iss_long = data["iss_position"]["longitude"]
-    const iss_lat = data["iss_position"]["latitude"]
-    setUrlMap(`https://sk.mapy.cz/zakladni?x=${iss_long}&y=${iss_lat}&z=8`) // cez template string to zadame do url adresy
+  if(loading) {
+    return <h2>Načítanie stránky</h2>
+  } else {
+    return <h1>Obsah stránky</h1>
   }
 
-  useEffect ( () => { // pôjde to až úplne na konci
-    getCoordinates() // zavolanie funkcie
-  }, []) // zavolanie funkcie len raz
-
-  
-
-  
-
-  return (        // 2. zavolanie return
-    <div>
-      <h1>API</h1>
-      <h2>Zemepisná šírka</h2>
-      <p>{latitude}</p>
-      <h2>Zemepisná dĺžka</h2> 
-      <p>{longitude}</p>
-      <a href={urlMap} target="_blank">Pozícia iss v mapách</a>
-    </div>
-  )
 }
 
 export default App
