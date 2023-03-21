11b377b5296e9d1630d65f1d35b2fec30212532b
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2023-03-21 22:04:32 +0100

    React 75 - Vypisujeme akt. data z API do stránky

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// React 74 - lepšia práca s API
+// React 75 - Vypisujeme aktuální data z API do stránky
 // druhy return nebude nikdy spustený = unreacheable code = nedostiahnuteľný kód
 
 import { useState, useEffect } from "react"
@@ -6,7 +6,9 @@ import { useState, useEffect } from "react"
 const url = "http://api.open-notify.org/iss-now.json"
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
+  const [latitude, setLatitude] = useState("")
+  const [longitude, setLongitude] = useState("")
 
 
 
@@ -14,19 +16,26 @@ const App = () => {
     fetch(url).then( (response) => response.json())
               .then( (data) => data["iss_position"]) 
               .then( (position) => {
-                  console.log(position["latitude"])
-                  console.log(position["longitude"])
+                  // console.log(position["latitude"])
+                  // console.log(position["longitude"])
+                  setLatitude(position["latitude"])
+                  setLongitude(position["longitude"])
               })
 
   }, []) 
 
-
-
-  if(loading) {
+  if(loading){
     return <h2>Načítanie stránky</h2>
-  } else {
-    return <h1>Obsah stránky</h1>
   }
+
+
+  // <></> dáva sa to namiesto div
+    return <>  <h2>Zemepisná šírka</h2>  
+        <p>{latitude}</p>
+        <h2>Zemepisná dĺžka</h2>
+        <p>{longitude}</p>
+    </>
+  
 
 }
 
