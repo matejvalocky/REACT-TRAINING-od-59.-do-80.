4151585374c00cc363a4faa37f6f274b5470c90b
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2023-03-21 21:54:37 +0100

    React 74 - lepšia práca s API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-// React 73 - Multiple Returns
+// React 74 - lepšia práca s API
 // druhy return nebude nikdy spustený = unreacheable code = nedostiahnuteľný kód
 
+import { useState, useEffect } from "react"
+
+const url = "http://api.open-notify.org/iss-now.json"
 
 const App = () => {
-  const loading = false
+  const [loading, setLoading] = useState(true)
+
+
+
+  useEffect( () => {
+    fetch(url).then( (response) => response.json())
+              .then( (data) => data["iss_position"]) 
+              .then( (position) => {
+                  console.log(position["latitude"])
+                  console.log(position["longitude"])
+              })
+
+  }, []) 
+
+
 
   if(loading) {
     return <h2>Načítanie stránky</h2>
