660e1cae599e288f1b3543ea6bc11e640d2f726c
Matej Valocký <74997007+matejvalocky@users.noreply.github.com>
2023-03-24 20:42:39 +0100

    React 78 - Short circuit evaluation 2. časť

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-// React 77 - Short circuit evaluation
+// React 78 - Short circuit evaluation 2. časť
 
 const App = () => {
   const value = "david"
-  const value2 = "harry"
+  const value2 = 1
 
   const result1 = value && value2
-  // prvý je true a druhé je akékoľvek = vracia sa druhá hodnota
-  // prvý je false a druhé akokoľvek = vracia sa prvá hodnota
+  // prvý je true = vracia sa druhá hodnota
+  // prvý je false = vracia sa prvá hodnota
   console.log(result1)
 
   const result2 = value || "tomas"
-  // prvý je true a druhé akékoľvek = vracia sa prvá hodnota
+  // prvý je true = vracia sa prvá hodnota
   // prvý je false, tak vracia sa druhá hodnota
-  console.log(result2)
+  // console.log(result2)
 
 
 
 
   return <div>
-    
+    <h1>{value2 || "Defaultný text"}</h1>
+    {value2 && <h1>Supertajný nadpis</h1>}
   </div>
 
   
